156e006f2647e4d83b4bb125e9dcd79f84abb0e0
toniadm <toniadm@swbell.net>
2018-11-09 21:45:47 -0600

    Search development

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import SearchPage from './SearchPage';
-import MainPage from './MainPage';
+import Search from './components/Search';
+import Main from './components/Main';
 
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    myBooks: []
+    books: []
   }
-  
+
   componentDidMount() {
-    BooksAPI.getAll().then((myBooks) => {
-      this.setState({ myBooks: myBooks })
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
     })
   }
+
+  moveShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf);
+
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <MainPage 
-          myBooks={this.state.myBooks}
-        />
+        {/*<Main
+          books={this.state.books}
+          moveShelf={this.moveShelf}
+        />*/}
+        <Search />
       </div>
     )
   }
